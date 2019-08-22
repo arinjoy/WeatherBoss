87a3bf88571a2fb46e95d35439b4a6bdcff570c5
Arinjoy Biswas <arinjoy.biswas@suncorp.com.au>
2019-08-23 08:06:02 +1000

    Initial Commit

---
+++ WeatherBoss/FirstViewController.swift
@@ -0,0 +1,20 @@
+//
+//  FirstViewController.swift
+//  WeatherBoss
+//
+//  Created by BISWAS, Arinjoy on 23/8/19.
+//  Copyright © 2019 ArinAppy. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ WeatherBoss/SecondViewController.swift
@@ -0,0 +1,20 @@
+//
+//  SecondViewController.swift
+//  WeatherBoss
+//
+//  Created by BISWAS, Arinjoy on 23/8/19.
+//  Copyright © 2019 ArinAppy. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
