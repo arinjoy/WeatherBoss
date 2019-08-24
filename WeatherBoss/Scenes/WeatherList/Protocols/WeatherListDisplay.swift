90540b449bc3fd27b94189e0c451397edf0d07a3
Arinjoy Biswas <arinjoy.biswas@suncorp.com.au>
2019-08-24 13:52:45 +1000

    added missing file to git

---
+++ WeatherBoss/Scenes/WeatherList/Protocols/WeatherListDisplay.swift
@@ -0,0 +1,33 @@
+//
+//  WeatherListDisplay.swift
+//  WeatherBoss
+//
+//  Created by BISWAS, Arinjoy on 24/8/19.
+//  Copyright © 2019 ArinAppy. All rights reserved.
+//
+
+import Foundation
+
+typealias WeatherListDataSource = DataSource<DataSection<WeatherSummaryPresentationItem>>
+
+protocol WeatherListDisplay: class {
+    
+    /// Will load the view title in navigation bar
+    ///
+    /// - Parameter title: The title to set
+    func setTitle(_ title: String)
+    
+    /// Will display the given data source as the primary display set
+    ///
+    /// - Parameter dataSource: The set of data to display (transformed view models)
+    func setWeatherListDataSource(_ dataSource: WeatherListDataSource)
+    
+    /// Will show loading indicator while weather is fetched
+    func showLoadingIndicator()
+    
+    /// Will hide the loading indicator
+    func hideLoadingIndicator()
+    
+    /// Will show an error alert
+    func showError(title: String, message: String, dismissTitle: String)
+}
